cc29e7213c21998a81bf4c0c27eef7f5fc3cc518
Reece <reecemorgandev@gmail.com>
2023-07-31 10:03:34 +0100

    Update player.gd

--- scripts/player.gd
+++ scripts/player.gd
@@ -25,7 +25,9 @@ func _physics_process(delta):
 			velocity.y = jump_velocity
 		elif not has_double_jumped:
 			# Double Jump
-			velocity.y = double_jump_velocity
+			# += instead of = to add to velocity of first jump
+			# enables higher double jump
+			velocity.y += double_jump_velocity
 			has_double_jumped = true
 
 	# Get the input direction and handle the movement/deceleration
