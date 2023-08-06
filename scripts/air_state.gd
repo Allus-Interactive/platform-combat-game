5dcc334aad5060ef9c797c83c8d9aaad77f75319
Reece <reecemorgandev@gmail.com>
2023-08-06 15:02:56 +0100

    added landing state

--- scripts/air_state.gd
+++ scripts/air_state.gd
@@ -2,22 +2,24 @@ extends PlayerState
 
 class_name Airstate
 
-@export var ground_state : PlayerState
+@export var landing_state : PlayerState
 @export var double_jump_velocity : float = -150.0
 @export var double_jump_animation : String = "double_jump"
+@export var landing_animation : String = "landing"
 
 var has_double_jumped : bool = false
 
 func state_process(_delta):
 	if character.is_on_floor():
-		next_state = ground_state
+		next_state = landing_state
 
 func state_input(event : InputEvent):
 	if event.is_action_pressed("jump") && not has_double_jumped:
 		double_jump()
 
 func on_exit():
-	if next_state == ground_state:
+	if next_state == landing_state:
+		playback.travel(landing_animation)
 		has_double_jumped = false
 
 func double_jump():
