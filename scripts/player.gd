ea253de6888864646185a0f7d4fa41216ab1d221
Reece <reecemorgandev@gmail.com>
2023-08-06 13:07:39 +0100

    implemented animation player and tree

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,7 +5,8 @@ extends CharacterBody2D
 @export var jump_velocity : float = -200.0
 @export var double_jump_velocity : float = -150.0
 
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var sprite : Sprite2D = $Sprite2D
+@onready var animation_tree : AnimationTree = $AnimationTree
 
 # Get the gravity from the project settings to be synced with RigidBody nodes
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -17,6 +18,9 @@ var is_at_jump_peak : bool = false
 var is_falling : bool = false
 var is_facing_right : bool = true
 var has_weapon : bool = false
+
+func _ready():
+	animation_tree.active = true
 
 var direction : Vector2 = Vector2.ZERO
 
@@ -47,13 +51,13 @@ func _physics_process(delta):
 	if not is_on_floor():
 		if velocity.y > -25 && velocity.y < 25 && not is_at_jump_peak:
 			# Player is at peak of jump
-			animated_sprite.play("jump_middle")
+			#animated_sprite.play("jump_middle")
 			animation_locked = true
 			is_at_jump_peak = true
 		
 		if velocity.y > 25 && not is_falling:
 			# Player is falling
-			animated_sprite.play("jump_end")
+			#animated_sprite.play("jump_end")
 			animation_locked = true
 			is_falling = true
 	
@@ -62,16 +66,16 @@ func _physics_process(delta):
 		roll()
 	
 	# Handle Weapon Toggle
-	if Input.is_action_just_pressed("toggle_weapon") && animated_sprite.animation != "roll":
-		toggle_weapon()
+	#if Input.is_action_just_pressed("toggle_weapon") && animated_sprite.animation != "roll":
+	#	toggle_weapon()
 	
 	# Get the input direction and handle the movement/deceleration
 	direction = Input.get_vector("move_left", "move_right", "ui_up", "ui_down")
 	
-	if direction.x != 0 && animated_sprite.animation != "jump_land" && animated_sprite.animation != "roll":
+	if direction.x != 0:
 		velocity.x = direction.x * speed
-	elif animated_sprite.animation == "roll":
-		pass
+	#elif animated_sprite.animation == "roll":
+		#pass
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 	
@@ -80,34 +84,23 @@ func _physics_process(delta):
 	update_direction()
 
 func update_animation():
-	if not animation_locked:
-		if not has_weapon:
-			if direction.x != 0:
-				animated_sprite.play("run")
-			else:
-				animated_sprite.play("idle")
-		else:
-			if direction.x != 0:
-				animated_sprite.play("run_sword")
-			else:
-				animated_sprite.play("idle_sword")
-			
+	animation_tree.set("parameters/Move/blend_position", direction.x)
 
 func update_direction():
 	if direction.x > 0:
 		# Player facing right
 		is_facing_right = true
-		animated_sprite.flip_h = false
+		sprite.flip_h = false
 	elif direction.x < 0:
 		# Player facing left
 		is_facing_right = false
-		animated_sprite.flip_h = true
+		sprite.flip_h = true
 
 func jump():
 	# Set y velocity to jump
 	velocity.y = jump_velocity
 	# Play jump start anim and lock anims
-	animated_sprite.play("jump_start")
+	#animated_sprite.play("jump_start")
 	animation_locked = true
 
 func double_jump():
@@ -119,16 +112,16 @@ func double_jump():
 	is_at_jump_peak = false
 	is_falling = false
 	# Play start anim and lock anims
-	animated_sprite.play("jump_start")
+	#animated_sprite.play("jump_start")
 	animation_locked = true
 
 func land():
-	animated_sprite.play("jump_land")
+	#animated_sprite.play("jump_land")
 	animation_locked = true
 
 func roll():
 	# Play roll anim and lock anims
-	animated_sprite.play("roll")
+	#animated_sprite.play("roll")
 	animation_locked = true
 	# Set velocity based on direction player is facing
 	if is_facing_right:
@@ -142,13 +135,13 @@ func toggle_weapon():
 	else:
 		has_weapon= true
 
-func _on_animated_sprite_2d_animation_finished():
-	# Call when any anim finishes
-	if animated_sprite.animation == "jump_land":
-		# Only run this when jump land anim is finished
-		animation_locked = false
-	
-	if animated_sprite.animation == "roll":
-		# Only run this when roll anim is finished
-		animation_locked = false
+#func _on_animated_sprite_2d_animation_finished():
+#	# Call when any anim finishes
+#	if animated_sprite.animation == "jump_land":
+#		# Only run this when jump land anim is finished
+#		animation_locked = false
+#
+#	if animated_sprite.animation == "roll":
+#		# Only run this when roll anim is finished
+#		animation_locked = false
 
