5a8b819d11ab300bc83bf34d0ea0c84f116b65cf
Reece <reecemorgandev@gmail.com>
2023-08-06 13:22:11 +0100

    created state machine scripts

---
+++ scripts/character_state_machine.gd
@@ -0,0 +1,21 @@
+extends Node
+
+class_name CharacterStateMachine
+
+@export var current_state : PlayerState
+
+var states : Array[PlayerState]
+
+func _ready():
+	for child in get_children():
+		if child is PlayerState:
+			states.append(child)
+			
+			# Set the states up with what they need to function
+			
+		else:
+			push_warning("Child " + child.name + " is not a State for CharacterStateMachine")
+
+func check_if_can_move():
+	return current_state.can_move
+
---
+++ scripts/player_state.gd
@@ -0,0 +1,5 @@
+extends Node
+
+class_name PlayerState
+
+@export var can_move : bool = true
