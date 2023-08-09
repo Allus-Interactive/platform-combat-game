2e877c6fdcd243abb097f2519de91c2f068c7bf6
Reece <reecemorgandev@gmail.com>
2023-08-09 12:31:08 +0100

    added basic enemy movement

--- scenes/enemies/snail.gd
+++ scenes/enemies/snail.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 @onready var animation_tree : AnimationTree = $AnimationTree
 
-const SPEED = 300.0
+@export var starting_move_direction : Vector2 = Vector2.LEFT
+@export var movement_speed : float = 30.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -15,12 +16,11 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-#	var direction = Input.get_axis("ui_left", "ui_right")
-#	if direction:
-#		velocity.x = direction * SPEED
-#	else:
-#		velocity.x = move_toward(velocity.x, 0, SPEED)
+	# Set direction to the starting move direction vector
+	var direction : Vector2 = starting_move_direction
+	if direction:
+		velocity.x = direction.x * movement_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, movement_speed)
 
 	move_and_slide()
