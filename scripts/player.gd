009da591d4892c4dbd75886019b2b93baeebcd51
Reece <reecemorgandev@gmail.com>
2023-08-11 09:57:32 +0100

    flip sword hitbox based on player direction

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+class_name Player
+
 @export var speed : float = 200.0
 
 @onready var sprite : Sprite2D = $Sprite2D
@@ -8,11 +10,12 @@ extends CharacterBody2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var direction : Vector2 = Vector2.ZERO
+
+signal facing_direction_changed(facing_right : bool)
 
 func _ready():
 	animation_tree.active = true
-
-var direction : Vector2 = Vector2.ZERO
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -41,3 +44,6 @@ func update_direction():
 		sprite.flip_h = false
 	elif direction.x < 0:
 		sprite.flip_h = true
+	
+	emit_signal("facing_direction_changed", !sprite.flip_h)
+
--- scripts/sword.gd
+++ scripts/sword.gd
@@ -1,9 +1,12 @@
 extends Area2D
 
 @export var damage : int = 10
+@export var player : Player
+@export var facing_shape : FacingCollisionShape2D
 
 func _ready():
 	monitoring = false
+	player.connect("facing_direction_changed", _on_player_facing_direction_changed)
 
 func _on_body_entered(body):
 	for child in body.get_children():
@@ -19,3 +22,8 @@ func _on_body_entered(body):
 			else:
 				child.hit(damage, Vector2.ZERO)
 
+func _on_player_facing_direction_changed(facing_right : bool):
+	if facing_right:
+		facing_shape.position = facing_shape.facing_right_postion
+	else:		
+		facing_shape.position = facing_shape.facing_left_postion
