bff2d4175db8c2fb1b337ae9a8a99d246f09f5ff
Reece <reecemorgandev@gmail.com>
2023-08-11 09:46:58 +0100

    added directional knockback to enemy

--- scripts/enemies/damageable.gd
+++ scripts/enemies/damageable.gd
@@ -2,7 +2,7 @@ extends Node
 
 class_name Damageable
 
-signal on_hit(node : Node, damage_taken : int)
+signal on_hit(node : Node, damage_taken : int, knockback_direction : Vector2)
 
 @export var dead_animation : String = "dead"
 @export var health : int = 20 : 
@@ -12,10 +12,10 @@ signal on_hit(node : Node, damage_taken : int)
 		SignalBus.emit_signal("on_health_changed", get_parent(), value - health)
 		health = value
 
-func hit(damage : int):
+func hit(damage : int, knockback_direction : Vector2):
 	health -= damage
 	
-	emit_signal("on_hit", get_parent(), damage)
+	emit_signal("on_hit", get_parent(), damage, knockback_direction)
 
 func _on_animation_tree_animation_finished(anim_name):
 	if (anim_name == dead_animation):
--- scripts/enemies/hit_state.gd
+++ scripts/enemies/hit_state.gd
@@ -6,7 +6,7 @@ class_name HitState
 @export var dead_state : PlayerState
 @export var return_state : PlayerState
 @export var dead_animation : String = "dead"
-@export var knockback_velocity : Vector2 = Vector2(100, 0)
+@export var knockback_speed : float = 100.0
 
 @onready var timer : Timer = $Timer
 
@@ -14,11 +14,11 @@ func _ready():
 	damageable.connect("on_hit", on_damagable_hit)
 
 func on_enter():
-	character.velocity = knockback_velocity
 	timer.start()
 
-func on_damagable_hit(_node : Node, _damage_amount : int):
+func on_damagable_hit(_node : Node, _damage_amount : int, knockback_direction : Vector2):
 	if damageable.health > 0:
+		character.velocity = knockback_speed * knockback_direction
 		emit_signal("interrupt_state", self)
 	else:
 		emit_signal("interrupt_state", dead_state)
