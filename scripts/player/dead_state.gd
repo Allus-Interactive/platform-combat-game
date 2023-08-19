21e66deeee4a886f59098f5cc544e9caa3c3f31f
Reece <reecemorgandev@gmail.com>
2023-08-19 10:00:08 +0100

    reload current scene on player death

--- scripts/player/dead_state.gd
+++ scripts/player/dead_state.gd
@@ -2,7 +2,7 @@ extends State
 
 class_name DeadState
 
-@export var scene_after_death : PackedScene
+# @export var scene_after_death : PackedScene
 
 @onready var timer : Timer = $Timer
 
@@ -12,7 +12,7 @@ func on_enter():
 func _on_timer_timeout():
 	pass
 	# TODO: reload current scene
-	
+	get_tree().reload_current_scene()
 #	print_debug("Scene after death: ", scene_after_death)
 #	if scene_after_death != null:
 #		get_tree().change_scene_to_packed(scene_after_death)
