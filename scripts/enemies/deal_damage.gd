4f0e283f2b47d410cd79a229f4057a0f6bee2913
Reece <reecemorgandev@gmail.com>
2023-08-19 10:33:07 +0100

    only deal damage if enemy is not dead

--- scripts/enemies/deal_damage.gd
+++ scripts/enemies/deal_damage.gd
@@ -1,9 +1,12 @@
 extends Area2D
 
 @export var damage : int = 5
+@export var dead_state : State
+
+@onready var state_machine : CharacterStateMachine = $"../CharacterStateMachine"
 
 # deals damage to player when enemy and player collide
 func _on_body_entered(body):
 	for child in body.get_children():
-		if child is PlayerDamageable:
+		if child is PlayerDamageable && state_machine.current_state != dead_state:
 			child.hit(damage, Vector2.LEFT)
